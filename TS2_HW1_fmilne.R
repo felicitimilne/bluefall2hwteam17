0e7be455546b8489032843e4b3fcfb3a94a9612e
felicitimilne <feliciti.d.milne@gmail.com>
2022-09-29 09:59:34 -0400

    rename

---
+++ TS2_HW1_fmilne.R
@@ -0,0 +1,7 @@
+# code for time series 2 homework 1 
+
+# libraries
+
+df <- read.csv("https://raw.githubusercontent.com/felicitimilne/bluefall2hwteam17/main/hrl_load_metered.csv?token=GHSAT0AAAAAABZLPKSLSFXGB4Z2N6IJDRJOYZVUJXA")
+
+summary(df)
