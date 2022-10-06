c31ddf9ad37e63cfad99e6c4062e526b3ea15e20
njohnso8 <njohnso8@ncsu.edu>
2022-10-06 08:45:40 -0400

    Full code for DM HW

--- njohnso8_Association_Analysis.R
+++ njohnso8_Association_Analysis.R
@@ -27,6 +27,7 @@ library(partykit)
 library(dbscan)
 #library(knitr)
 
+#Initial data transformation
 
 order <- read.csv("https://github.com/sjsimmo2/DataMining-Fall/blob/master/orderData.csv?raw=true", header = TRUE)
 
@@ -47,6 +48,8 @@ meat_list <- unique(wideorder$meat)
 wine_list <- unique(wideorder$wine)
 side_list <- unique(wideorder$side)
 
+#Plotting frequencies of individual items
+
 ggplot(data = wideorder, aes(x = reorder(meat, meat, function(x) - length(x)))) + geom_bar() + 
   theme(axis.text.x=element_text(angle = -60, hjust = 0), plot.title = element_text(hjust = 0.5)) +
   labs(x = "Item", y = "Frequency", title = "Descending Order of Meat Frequency")
@@ -59,7 +62,9 @@ ggplot(data = wideorder, aes(x = reorder(side, side, function(x) - length(x)))) 
   theme(axis.text.x=element_text(angle = -60, hjust = 0), plot.title = element_text(hjust = 0.5)) +
   labs(x = "Item", y = "Frequency", title = "Descending Order of Side Frequency")
 
+#Making the data transactional for apriori analysis
 trans.order <- as(split(order$item, order$OrderSeat), "transactions")
+
 # order.rules <- trans.order %>% apriori(parameter = list(supp = 0.01, conf = 0.25, target = "rules")) %>% sort(by = "confidence", decreasing = TRUE)
 # order.rules.df <- as.data.frame(inspect(order.rules))
 # order.rules.df <- order.rules.df %>% dplyr::select(lhs, rhs, support, confidence, coverage, lift, count) %>%
@@ -69,20 +74,38 @@ trans.order <- as(split(order$item, order$OrderSeat), "transactions")
 # ggplot(data = order.rules.df)
 # plot(head(order.rules, n = 10), method = "graph", engine = "htmlwidget")
 
-order.rules.meat <- trans.order %>% apriori(parameter = list(supp = 0.008, conf = 0.15, target = "rules"), appearance = list(default = "rhs", lhs = unique(wideorder$meat))) %>% sort(by = "lift", decreasing = TRUE)
+#Ensuring meat is on LHS and wine is on RHS, 0.008 determined through experimentation
+#Two df's: one ordered by confidence, the other by lift
+order.rules.meat <- trans.order %>% apriori(parameter = list(supp = 0.008, conf = 0.15, target = "rules"), appearance = list(default = "rhs", lhs = unique(wideorder$meat))) %>% sort(by = "confidence", decreasing = TRUE)
+order.rules.meat.lift <- trans.order %>% apriori(parameter = list(supp = 0.008, conf = 0.15, target = "rules"), appearance = list(default = "rhs", lhs = unique(wideorder$meat))) %>% sort(by = "lift", decreasing = TRUE)
 order.rules.meat.df <- as.data.frame(inspect(order.rules.meat)) 
+order.rules.meat.df.lift <- as.data.frame(inspect(order.rules.meat.lift)) 
+
+#Lose brackets, check if RHS is a wine and grab indices
 list = str_replace(str_replace(order.rules.meat.df$rhs, "\\{", ""), "\\}", "")
+list.lift = str_replace(str_replace(order.rules.meat.df.lift$rhs, "\\{", ""), "\\}", "")
 index = c()
+index.lift = c()
+
 for (i in 1:length(list)) { 
   if (list[i] %in% unique(wideorder$wine)) {
     index = append(index, i)
   }
+  if (list.lift[i] %in% unique(wideorder$wine)) {
+    index.lift = append(index.lift, i)
+  }
 }
+#Final meat to wine dataframes
 order.rules.meat.df <- order.rules.meat.df[index,]
+order.rules.meat.df.lift <- order.rules.meat.df.lift[index.lift,]
 
-unique.meats.df.lift <- order.rules.meat.df[match(unique(order.rules.meat.df$lhs), order.rules.meat.df$lhs),]
+#Ensuring only the meat with the highest confidence/lift is represented
+unique.meats.df <- order.rules.meat.df[match(unique(order.rules.meat.df$lhs), order.rules.meat.df$lhs),]
+unique.meats.df <- unique.meats.df[1:length(unique(wideorder$meat)),]
+unique.meats.df.lift <- order.rules.meat.df.lift[match(unique(order.rules.meat.df.lift$lhs), order.rules.meat.df.lift$lhs),]
 unique.meats.df.lift <- unique.meats.df.lift[1:length(unique(wideorder$meat)),]
 
+#Plot results
 plot(order.rules.meat.df)
 plot(unique.meats.df)
 plot(unique.meats.df.lift)
