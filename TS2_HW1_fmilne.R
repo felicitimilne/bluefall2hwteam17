15c514df81906ee0d26b93bfd78f34435582382a
felicitimilne <feliciti.d.milne@gmail.com>
2022-10-08 10:12:09 -0400

    current work

--- TS2_HW1_fmilne.R
+++ TS2_HW1_fmilne.R
@@ -2,8 +2,36 @@
 
 # libraries
 library(dplyr)
+library(ggplot2)
 
 #load data
 df <- read.csv("https://raw.githubusercontent.com/felicitimilne/bluefall2hwteam17/main/hrl_load_metered.csv")
 head(df)
 
+#reformat date column and drop hours
+df$datetime_beginning_ept <- as.Date(df$datetime_beginning_ept, "%m/%d/%y")
+head(df)
+
+#roll up hourly data into daily and drop unnecessary columns
+rolled_df <- df %>% 
+  group_by(datetime_beginning_ept) %>% 
+  summarise_at(vars(mw),  list(min, mean, max))
+
+#rename columns
+colnames(rolled_df)[colnames(rolled_df) == "fn1"] = "mw_min"
+colnames(rolled_df)[colnames(rolled_df) == "fn2"] = "mw_mean"
+colnames(rolled_df)[colnames(rolled_df) == "fn3"] = "mw_max"
+
+#create time series object 
+MegaWatt_mean <- ts(data = rolled_df$mw_mean, start = c(2019, 8), frequency = 12)
+
+autoplot(MegaWatt_mean) +
+  labs(title="Time Series plot for Enegry Usage", x = "Date", y="Megawatts") +
+
+#get current labels
+old_x <- load("https://raw.githubusercontent.com/felicitimilne/bluefall2hwteam17/main/old_x.Rda")
+
+
+
+#scale_x_continuous(breaks = c(2020, 2040, 2060, 2080, 2100, 2120), 
+                     #labels = c("-5000", "-2500", "TSS", "2500", "5000"))
