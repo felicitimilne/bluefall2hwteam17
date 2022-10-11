bf6d3ebd5a51e1250b1e59d1b83fa8c5c8ffc18d
patrick okeefe <pjokeefe@ncsu.edu>
2022-10-10 22:31:39 -0400

    Updated ARIMA Models

--- TS2_HW1_pjokeefe.R
+++ TS2_HW1_pjokeefe.R
@@ -22,8 +22,10 @@ library(prophet)
 df <- read.csv("https://raw.githubusercontent.com/felicitimilne/bluefall2hwteam17/main/hrl_load_metered.csv")
 head(df)
 
+#get rid of useless variables
 df <- df[,c(1,6)]
 
+#Change variable to a date time object
 df$datetime_beginning_ept <- mdy_hm(df$datetime_beginning_ept, tz = Sys.timezone())
 
 #Impute the average of previous and next observation to fix the zeros for DLS
@@ -42,17 +44,43 @@ autoplot(energy) +
   xlab("Time") +
   ylab("Energy")
 
-
+#decomposition plot
 decomp_stl <- stl(energy, s.window = 7)
 plot(decomp_stl)
 autoplot(decomp_stl)
 
+#subseries plot that plots the averages of the seasons
 ggsubseriesplot(energy)
 
+#Create the holt winter's model
 HW <- hw(energy, seasonal = "additive", h = 168)
 summary(HW)
 
+#Plot the forecasts from the holt-winters model
 autoplot(HW)+
   autolayer(fitted(HW),series="Fitted")+ylab("US Steel Shipments")
 
+#nsdiffs to check for stochastic or deterministic seasonality
+energy %>% nsdiffs()
 
+#ndiffs to check for stationarity
+energy %>% diff(lag = 24) %>% ndiffs()
+
+energy %>% diff(lag = 24) %>% ggtsdisplay()
+
+# Seasonal ARIMA with seasonal differences
+#testing AR and MA terms
+energy %>% 
+  Arima(order=c(1,0,3), seasonal=c(2,1,3)) %>%
+  residuals() %>% ggtsdisplay()
+
+#Auto arima with seasonal differences
+S.ARIMA <- auto.arima(energy, method="ML", seasonal = TRUE, D = 1, stepwise = TRUE)
+
+summary(S.ARIMA)
+
+energy %>% 
+  Arima(order=c(0,0,2), seasonal=c(0,1,2)) %>%
+  residuals() %>% ggtsdisplay()
+ggAcf(S.ARIMA$residuals)
+ggPacf(S.ARIMA$residuals)
