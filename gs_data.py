aa053a2b80a0edcdf2c990a08c2596a380a57bea
njohnso8 <njohnso8@ncsu.edu>
2022-10-17 09:43:39 -0400

    Noah push

--- gs_data.py
+++ gs_data.py
@@ -61,3 +61,90 @@ gsi_grouped.to_csv("/Users/noahjohnson/Downloads/bluefall2hwteam17/gsi_monthly.c
 gsi_yr_grouped.to_csv("/Users/noahjohnson/Downloads/bluefall2hwteam17/gsi_yearly.csv")
 mit_grouped.to_csv("/Users/noahjohnson/Downloads/bluefall2hwteam17/mit_monthly.csv")
 mit_yr_grouped.to_csv("/Users/noahjohnson/Downloads/bluefall2hwteam17/mit_yearly.csv")
+
+song_df = pd.read_csv("/Users/noahjohnson/Downloads/bluefall2hwteam17/lyrics_with_wc1.csv")
+song_df = song_df[["Unnamed: 0", "Song & Artist", "Word Count"]]
+lyric_df = pd.read_csv("/Users/noahjohnson/Downloads/bluefall2hwteam17/sent_emo_lyrics.csv")
+
+
+yearly_wc = []
+yearly_fw = []
+yearly_comp = []
+yearly_ps = []
+yearly_ns = []
+yearly_ef = []
+yearly_anger = []
+yearly_antic = []
+yearly_disg = []
+yearly_fear = []
+yearly_joy = []
+yearly_neg = []
+yearly_pos = []
+yearly_sad = []
+yearly_surp = []
+yearly_trust = []
+yearly_ev = []
+
+for i in range(16):
+    song_list = list(song_df.iloc[100 * i : 100 * (i + 1)]["Song & Artist"])
+    
+    temp_wc_list = []
+    temp_fw_list = []
+    temp_comp_list = []
+    temp_ps_list = []
+    temp_ns_list = []
+    temp_ef_list = []
+    temp_anger_list = []
+    temp_antic_list = []
+    temp_disg_list = []
+    temp_fear_list = []
+    temp_joy_list = []
+    temp_neg_list = []
+    temp_pos_list = []
+    temp_sad_list = []
+    temp_surp_list = []
+    temp_trust_list = []
+    temp_ev_list = []
+    
+    for j in range(len(song_list)):
+        temp_partition = lyric_df.loc[lyric_df["Song & Artist"] == song_list[j]]
+        temp_wc_list.append(float(temp_partition["Word Count"].drop_duplicates()))
+        temp_fw_list.append(float(temp_partition["num_fw"].drop_duplicates()))
+        temp_comp_list.append(float(temp_partition["Compound Sentiment"].drop_duplicates()))
+        temp_ps_list.append(float(temp_partition["Positive Sentiment"].drop_duplicates()))
+        temp_ns_list.append(float(temp_partition["Negative Sentiment"].drop_duplicates()))
+        temp_ef_list.append(float(temp_partition["Emo Flag Total"].drop_duplicates()))
+        temp_anger_list.append(float(temp_partition["Anger"].drop_duplicates()))
+        temp_antic_list.append(float(temp_partition["Anticipation"].drop_duplicates()))
+        temp_disg_list.append(float(temp_partition["Disgust"].drop_duplicates()))
+        temp_fear_list.append(float(temp_partition["Fear"].drop_duplicates()))
+        temp_joy_list.append(float(temp_partition["Joy"].drop_duplicates()))
+        temp_neg_list.append(float(temp_partition["Positive"].drop_duplicates()))
+        temp_pos_list.append(float(temp_partition["Negative"].drop_duplicates()))
+        temp_sad_list.append(float(temp_partition["Sadness"].drop_duplicates()))
+        temp_surp_list.append(float(temp_partition["Surprise"].drop_duplicates()))
+        temp_trust_list.append(float(temp_partition["Trust"].drop_duplicates()))
+        temp_ev_list.append(float(temp_partition["Expanded Valence"].drop_duplicates()))
+    
+    yearly_wc.append(np.array(temp_wc_list).mean())
+    yearly_fw.append(np.array(temp_fw_list).mean())
+    yearly_comp.append(np.array(temp_comp_list).mean())
+    yearly_ps.append(np.array(temp_ps_list).mean())
+    yearly_ns.append(np.array(temp_ns_list).mean())
+    yearly_ef.append(np.array(temp_ef_list).mean())
+    yearly_anger.append(np.array(temp_anger_list).mean())
+    yearly_antic.append(np.array(temp_antic_list).mean())
+    yearly_disg.append(np.array(temp_disg_list).mean())
+    yearly_fear.append(np.array(temp_fear_list).mean())
+    yearly_joy.append(np.array(temp_joy_list).mean())
+    yearly_neg.append(np.array(temp_neg_list).mean())
+    yearly_pos.append(np.array(temp_pos_list).mean())
+    yearly_sad.append(np.array(temp_sad_list).mean())
+    yearly_surp.append(np.array(temp_surp_list).mean())
+    yearly_trust.append(np.array(temp_trust_list).mean())
+    yearly_ev.append(np.array(temp_ev_list).mean())
+    
+gs_df = pd.DataFrame(list(zip(list(gsi_yr_grouped["SGIXSENT"]), list(gsi_yr_grouped["SGXQIN01"]), ([0, 0, 0, 0, 0, 0] + list(mit_yr_grouped["SCORE"])))), columns = ["Avg GS Index", "Avg GS Indicator", "Avg Twitter Sent"])
+yearly_emo_stats_df = pd.DataFrame(list(zip(list(range(2006, 2022)), yearly_wc, yearly_fw, yearly_comp, yearly_ps, yearly_ns, yearly_ef, yearly_anger, yearly_antic, yearly_disg, yearly_fear, yearly_joy, yearly_neg, yearly_pos, yearly_sad, yearly_surp, yearly_trust, yearly_ev)), columns = ["Year", "Avg Word Count", "Avg Foreign Words", "Avg Comp Sent", "Avg Pos Sent", "Avg Neg Sent", "Avg Emo Flags", "Avg Anger", "Avg Antic", "Avg Disgust", "Avg Fear", "Avg Joy", "Avg Neg", "Avg Pos", "Avg Sadness", "Avg Surprise", "Avg Trust", "Avg Exp Valence"])
+yearly_emo_stats_df = pd.concat([yearly_emo_stats_df, gs_df], axis = 1)
+yearly_emo_stats_df.to_csv("/Users/noahjohnson/Downloads/bluefall2hwteam17/yearly_sent_metrics.csv")
