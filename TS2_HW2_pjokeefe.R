b62cf12f0ec8e137b08448425bbb04fae611e7e4
patrick okeefe <pjokeefe@ncsu.edu>
2022-10-17 22:18:49 -0400

    Time series 2 HW, Data read and Prophet model built

---
+++ TS2_HW2_pjokeefe.R
@@ -0,0 +1,90 @@
+# code for time series 2 homework 1 
+
+# libraries
+library(dplyr)
+library(ggfortify)
+library(lubridate)
+library(tseries)
+library(forecast)
+library(haven)
+library(fma)
+library(expsmooth)
+library(lmtest)
+library(zoo)
+library(seasonal)
+library(ggplot2)
+library(seasonalview)
+library(aTSA)
+library(imputeTS)
+library(prophet)
+
+#load data
+df <- read.csv("https://raw.githubusercontent.com/felicitimilne/bluefall2hwteam17/main/hrl_load_metered.csv")
+head(df)
+
+df2 <- read.csv("https://github.com/felicitimilne/bluefall2hwteam17/raw/main/hrl_load_metered%20-%20test1.csv")
+head(df2)
+
+
+df3 <- rbind(df,df2)
+
+validation <- read.csv("https://github.com/felicitimilne/bluefall2hwteam17/raw/main/hrl_load_metered%20-%20test2.csv")
+
+#get rid of useless variables
+df3 <- df[,c(1,6)]
+
+validation <- validation[,c(1,6)]
+
+#Change variable to a date time object
+df3$datetime_beginning_ept <- mdy_hm(df3$datetime_beginning_ept, tz = Sys.timezone())
+
+validation$datetime_beginning_ept <- mdy_hm(validation$datetime_beginning_ept, tz = Sys.timezone())
+
+#Impute the average of previous and next observation to fix the zeros for DLS
+df3[c(5280:5290),]
+df3[5283,2] <- 904.2965
+
+df3[c(14180:14190),]
+df3[14187,2] <- 844.047
+
+# create time series object
+energy <- ts(df3[,2], start = 2019, frequency = 24) # frequency = 24 hours * 365.25 days in a year
+
+# autoplot
+autoplot(energy) +
+  ggtitle("Energy Usage") +
+  xlab("Time") +
+  ylab("Energy")
+
+#decomposition plot
+decomp_stl <- stl(energy, s.window = 7)
+plot(decomp_stl)
+autoplot(decomp_stl)
+
+#subseries plot that plots the averages of the seasons
+ggsubseriesplot(energy)
+
+# Prophet
+
+prophet.data <- data.frame(ds = df3$datetime_beginning_ept, y = df3$mw)
+
+Prof <- prophet()
+Prof <- add_country_holidays(Prof, "US")
+Prof <- add_seasonality(Prof, name='monthly', period=30.5, fourier.order=6)
+Prof <- fit.prophet(Prof, prophet.data)
+
+forecast.data <- make_future_dataframe(Prof, periods = 168, freq = 'hour')
+
+
+#plot(Prof, predict(Prof, forecast.data))
+
+# Calculate prediction errors from forecast
+
+Prophet.error <- validation - tail(predict(Prof, forecast.data)$yhat, 168)
+
+# Calculate prediction error statistics (MAE and MAPE)
+Prophet.MAE <- mean(abs(Prophet.error$mw))
+Prophet.MAPE <- mean(abs(Prophet.error$mw)/abs(validation$mw))*100
+
+Prophet.MAE
+Prophet.MAPE
